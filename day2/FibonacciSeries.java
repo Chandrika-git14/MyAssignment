b0cc3c971c49ad2563176f9bdb59493ed03706b7
Chandrika-git14 <chandrikaece14@gmail.com>
2025-05-26 22:37:14 +0530

    Add files via upload

---
+++ day2/FibonacciSeries.java
@@ -0,0 +1,19 @@
+package Week1.day2;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		
+		int a=0, b=1;
+		System.out.println("Fibonacci series upto 8 terms:");
+		for(int i = 0; i < 8 ;i++)
+			{
+		System.out.println(a+ "");
+		int c = a+b;
+		a=b;
+		b=c;
+			}
+
+	}
+
+}
---
+++ day2/IsPrimeNumber.java
@@ -0,0 +1,16 @@
+package Week1.day2;
+
+public class IsPrimeNumber {
+
+	public static void main(String[] args) {
+		int i=1;
+		if (i%2!=0 && i%i==0) {
+			System.out.println(i+" is prime number");
+		}
+		else {
+			System.out.println(i+" is not prime number");
+		}
+
+	}
+
+}
